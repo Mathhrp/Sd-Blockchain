a62fa35fb0e7cfdd3ecd21819060764236b2905a
Matheus Romano <matheus.henrique.rp@gmail.com>
2018-12-02 16:09:02 -0200

    sha256

--- NoobCoins/NoobCoins/StringUtil.cs
+++ NoobCoins/NoobCoins/StringUtil.cs
@@ -15,9 +15,19 @@ namespace NoobCoins
             {
                 HashAlgorithm sha = new SHA1CryptoServiceProvider();
                 byte[] has = sha.ComputeHash(Encoding.ASCII.GetBytes(imput));
+                StringBuilder hexString = new StringBuilder();
+                for(int i=0;i< has.Length;i++)
+                {
+                    string hex = has[i].ToString("X4");
+                    if (hex.Length == 1) hexString.Append('0');
+                    hexString.Append(hex);
+                }
+                return hexString.ToString();
             }
-            catch { }
-            return "";
+            catch (Exception e)
+            {
+                throw new Exception("erro",e);
+            }
         }
     }
 }
